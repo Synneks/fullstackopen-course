53777dca7d9a16b100bede42a5e600a95b692200
Denis Eneotescu <denis.eneotescu@web-dienstleister.de>
2023-01-27 23:25:30 +0200

    part1-1:3

--- part1/src/App.js
+++ part1/src/App.js
@@ -5,7 +5,7 @@ const Header = (props) => {
 const Part = (props) => {
     return (
         <p>
-            {props.info.part} {props.info.exercises}
+            {props.part.name} {props.part.exercises}
         </p>
     );
 };
@@ -13,9 +13,9 @@ const Part = (props) => {
 const Content = (props) => {
     return (
         <>
-            <Part info={props.info[0]} />
-            <Part info={props.info[1]} />
-            <Part info={props.info[2]} />
+            <Part part={props.parts[0]} />
+            <Part part={props.parts[1]} />
+            <Part part={props.parts[2]} />
         </>
     );
 };
@@ -31,24 +31,26 @@ const Total = (props) => {
 
 const App = () => {
     const course = "Half Stack application development";
-    const part1 = "Fundamentals of React";
-    const exercises1 = 10;
-    const part2 = "Using props to pass data";
-    const exercises2 = 7;
-    const part3 = "State of a component";
-    const exercises3 = 14;
+    const part1 = {
+        name: "Fundamentals of React",
+        exercises: 10,
+    };
+    const part2 = {
+        name: "Using props to pass data",
+        exercises: 7,
+    };
+    const part3 = {
+        name: "State of a component",
+        exercises: 14,
+    };
+
+    const parts = [part1, part2, part3];
 
     return (
         <div>
             <Header course={course} />
-            <Content
-                info={[
-                    { part: part1, exercises: exercises1 },
-                    { part: part2, exercises: exercises2 },
-                    { part: part3, exercises: exercises3 },
-                ]}
-            />
-            <Total exercises={[exercises1, exercises2, exercises3]} />
+            <Content parts={parts} />
+            <Total exercises={parts.map((part) => part.exercises)} />
         </div>
     );
 };
