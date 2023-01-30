3753fa63fb38a5c62884c29a3650aa455c0d0458
Denis Eneotescu <denis.eneotescu@web-dienstleister.de>
2023-01-30 15:54:19 +0200

    part1-1:12_anecdotes-step1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,83 +1,41 @@
 import { useState } from "react";
 
-const Feedback = ({ handleGood, handleNeutral, handleBad }) => {
-    return (
-        <>
-            <h1>Give Feedback</h1>
-            <FeedbackButton vote={handleGood} text="good" />
-            <FeedbackButton vote={handleNeutral} text="neutral" />
-            <FeedbackButton vote={handleBad} text="bad" />
-        </>
-    );
-};
-
-const FeedbackButton = ({ vote, text }) => {
-    return <button onClick={vote}>{text}</button>;
-};
-
-const Statistics = ({ votes }) => {
-    const votesAmount = votes.good + votes.neutral + votes.bad;
-    const average = (votes.good - votes.bad) / votesAmount;
-    const positives = (votes.good / votesAmount) * 100;
-
-    if (!votesAmount) {
-        return (
-            <>
-                <p>No feedback given</p>
-            </>
-        );
+const getDifferentRandomNumber = (current, length) => {
+    const newRandomNumber = Math.floor(Math.random() * length);
+    if (current === newRandomNumber) {
+        getDifferentRandomNumber(current, length);
     }
-
-    return (
-        <>
-            <h1>Statistics</h1>
-            <table>
-                <StatisticsLine text="good" value={votes.good} />
-                <StatisticsLine text="neutral" value={votes.neutral} />
-                <StatisticsLine text="bad" value={votes.bad} />
-                <StatisticsLine text="total" value={votesAmount} />
-                <StatisticsLine text="average" value={average} />
-                <StatisticsLine text="positive" value={positives} />
-            </table>
-        </>
-    );
-};
-
-const StatisticsLine = ({ text, value }) => {
-    return (
-        <tr>
-            <td>{text}</td>
-            <td>{value}</td>
-        </tr>
-    );
+    return newRandomNumber;
 };
 
 const App = () => {
-    // save clicks of each button to its own state
-    const [good, setGood] = useState(0);
-    const [neutral, setNeutral] = useState(0);
-    const [bad, setBad] = useState(0);
+    const anecdotes = [
+        "If it hurts, do it more often.",
+        "Adding manpower to a late software project makes it later!",
+        "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+        "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+        "Premature optimization is the root of all evil.",
+        "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+        "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+        "The only way to go fast, is to go well.",
+    ];
 
-    const handleGood = () => {
-        setGood(good + 1);
-    };
-
-    const handleNeutral = () => {
-        setNeutral(neutral + 1);
-    };
-
-    const handleBad = () => {
-        setBad(bad + 1);
-    };
+    const [selected, setSelected] = useState(0);
 
     return (
         <div>
-            <Feedback
-                handleGood={handleGood}
-                handleNeutral={handleNeutral}
-                handleBad={handleBad}
-            />
-            <Statistics votes={{ good, neutral, bad }} />
+            <div style={{ height: 50 }}>
+                <p>{anecdotes[selected]}</p>
+            </div>
+            <button
+                onClick={() =>
+                    setSelected(
+                        getDifferentRandomNumber(selected, anecdotes.length)
+                    )
+                }
+            >
+                Next
+            </button>
         </div>
     );
 };
