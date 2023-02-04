0acbef3db88699da6e4234a2d0b4541350c5e38c
Denis Eneotescu <denis.eneotescu@web-dienstleister.de>
2023-02-04 15:32:46 +0200

    part2-2:6_phonebook-step1

--- part2-notes-part2-1/src/App.js
+++ part2-notes-part2-1/src/App.js
@@ -1,18 +1,40 @@
-import Note from './components/Note'
+import { useState } from "react";
 
-const App = ({ notes }) => {
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleNameInput = (e) => {
+    setNewName(e.target.value);
+  };
+  const addToPhonebook = (e) => {
+    e.preventDefault();
+    const newPerson = { name: newName };
+    setPersons(persons.concat(newPerson));
+    setNewName("");
+  };
+
   return (
     <div>
-      <h1>Notes</h1>
+      <h2>Phonebook</h2>
+      <form onSubmit={addToPhonebook}>
+        <div>
+          name:{" "}
+          <input name="nameInput" value={newName} onChange={handleNameInput} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
       <ul>
-        <ul>
-          {notes.map(note => 
-            <Note key={note.id} note={note} />
-          )}
-        </ul>
+        {persons.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
       </ul>
+      {newName !== "" && <p>Name is {newName}.</p>}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
