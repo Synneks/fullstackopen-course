a14ccdd56a3399664537f3478bf3c53a769b5039
Denis Eneotescu <denis.eneotescu@web-dienstleister.de>
2023-02-04 16:32:36 +0200

    part2-2:8_phonebook-step3

--- part2-notes-part2-1/src/App.js
+++ part2-notes-part2-1/src/App.js
@@ -1,20 +1,32 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "123" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleNameInput = (e) => {
     setNewName(e.target.value);
   };
+  const handleNumberInput = (e) => {
+    setNewNumber(e.target.value);
+  };
   const addToPhonebook = (e) => {
     e.preventDefault();
+    if (!newName || !newNumber) {
+      !newName && setNewName("");
+      !newNumber && setNewNumber("");
+      return alert("Both fields are mandatory.");
+    }
     if (persons.find((person) => person.name === newName)) {
       return alert(`${newName} has already been added`);
     }
-    const newPerson = { name: newName };
+    const newPerson = { name: newName, number: newNumber };
     setPersons(persons.concat(newPerson));
     setNewName("");
+    setNewNumber("");
   };
 
   return (
@@ -24,6 +36,13 @@ const App = () => {
         <div>
           name:{" "}
           <input name="nameInput" value={newName} onChange={handleNameInput} />
+          <br />
+          number:{" "}
+          <input
+            name="numberInput"
+            value={newNumber}
+            onChange={handleNumberInput}
+          />
         </div>
         <div>
           <button type="submit">add</button>
@@ -32,10 +51,13 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map((person) => (
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}>
+            {person.name} - {person.number}
+          </li>
         ))}
       </ul>
-      {newName !== "" && <p>Name is {newName}.</p>}
+      {newName && <p>Name is {newName}.</p>}
+      {newNumber && <p>Number is {newNumber}.</p>}
     </div>
   );
 };
