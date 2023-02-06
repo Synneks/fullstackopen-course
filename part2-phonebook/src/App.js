116f683a5a519bd3ce1279938787eba8158fcbc9
Denis Eneotescu <denis.eneotescu@web-dienstleister.de>
2023-02-06 17:17:55 +0200

    part2:2-11_phonebook-step6 added useEffect hook

--- part2-phonebook/src/App.js
+++ part2-phonebook/src/App.js
@@ -1,16 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "redaxios";
 import Persons from "./components/Persons";
 import { PhonebookForm } from "./components/PhonebookForm";
 import Search from "./components/Search";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [filteredList, setFilteredList] = useState(persons);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((res) => {
+      setPersons(res.data);
+      setFilteredList(res.data);
+    });
+  }, []);
 
   return (
     <div>
