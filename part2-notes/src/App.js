328b9275fd7942457ecbc62c8c907e7a262f4ba9
Denis Eneotescu <denis.eneotescu@web-dienstleister.de>
2023-02-09 13:21:21 +0200

    part2 altering data in server

--- part2-notes/src/App.js
+++ part2-notes/src/App.js
@@ -2,37 +2,42 @@ import { useEffect, useState } from "react";
 import axios from "redaxios";
 import Note from "./components/Note";
 
+export const BASE_URL = "http://localhost:3001";
+
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
-    console.log("effect");
-    axios.get("http://localhost:3001/notes").then((response) => {
-      console.log("promise fulfilled");
+    axios.get(BASE_URL + "/notes").then((response) => {
       setNotes(response.data);
     });
   }, []);
-  console.log("render", notes.length, "notes");
 
   const addNote = (event) => {
     event.preventDefault();
     const noteObject = {
       content: newNote,
       important: Math.random() > 0.5,
-      id: notes.length + 1,
     };
 
-    setNotes(notes.concat(noteObject));
-    setNewNote("");
+    axios.post(BASE_URL + "/notes", noteObject).then((res) => {
+      console.log(res);
+      setNotes(notes.concat(res.data));
+      setNewNote("");
+    });
   };
 
   const handleNoteChange = (event) => {
     setNewNote(event.target.value);
   };
 
-  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+  const isNoteImportant = (note) => note.important;
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => isNoteImportant(note));
 
   return (
     <div>
