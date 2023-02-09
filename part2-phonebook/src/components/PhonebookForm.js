76966cba7e30870ab8fce33da8bf44e7bd6014ff
Denis Eneotescu <denis.eneotescu@web-dienstleister.de>
2023-02-09 19:07:34 +0200

    part2:2-15_phonebook-step10

--- part2-phonebook/src/components/PhonebookForm.js
+++ part2-phonebook/src/components/PhonebookForm.js
@@ -13,30 +13,50 @@ export const PhonebookForm = ({ contacts, setContacts, setFilteredList }) => {
     setNewNumber(e.target.value);
   };
 
+  const updateProps = (updatedContacts) => {
+    setContacts(updatedContacts);
+    setFilteredList(updatedContacts);
+    setNewName("");
+    setNewNumber("");
+  };
+
   const addToPhonebook = (e) => {
     e.preventDefault();
+
     if (!newName || !newNumber) {
       !newName && setNewName("");
       !newNumber && setNewNumber("");
       return alert("Both fields are mandatory.");
     }
 
-    if (contacts.find((contact) => contact.name === newName)) {
-      return alert(`${newName} has already been added`);
+    const foundUser = contacts.find((contact) => contact.name === newName);
+
+    if (foundUser) {
+      if (
+        !window.confirm(
+          `${newName} already exists in contact list. Do you want to update the number?`
+        )
+      ) {
+        return;
+      }
+      const modifiedContact = { ...foundUser, number: newNumber };
+      contactsService.updateContact(modifiedContact).then((updatedContact) => {
+        const updatedContacts = contacts.map((contact) =>
+          contact.id === updatedContact.id ? updatedContact : contact
+        );
+        updateProps(updatedContacts);
+      });
+    } else {
+      const newContact = {
+        name: newName,
+        number: newNumber,
+      };
+
+      contactsService.saveContact(newContact).then((savedContact) => {
+        const updatedContacts = contacts.concat(savedContact);
+        updateProps(updatedContacts);
+      });
     }
-
-    const newContact = {
-      name: newName,
-      number: newNumber,
-    };
-
-    contactsService.saveContact(newContact).then((savedContact) => {
-      const updatedContacts = contacts.concat(savedContact);
-      setContacts(updatedContacts);
-      setFilteredList(updatedContacts);
-      setNewName("");
-      setNewNumber("");
-    });
   };
 
   return (
--- part2-phonebook/src/services/contacts.js
+++ part2-phonebook/src/services/contacts.js
@@ -7,8 +7,18 @@ const getAllContacts = () => axios.get(BASE_URL).then((res) => res.data);
 const saveContact = (newContact) =>
   axios.post(BASE_URL, newContact).then((res) => res.data);
 
+const updateContact = (modifiedContact) =>
+  axios
+    .put(BASE_URL + `/${modifiedContact.id}`, modifiedContact)
+    .then((res) => res.data);
+
 const deleteContact = (contactId) => axios.delete(BASE_URL + `/${contactId}`);
 
-const contactsService = { getAllContacts, saveContact, deleteContact };
+const contactsService = {
+  getAllContacts,
+  saveContact,
+  updateContact,
+  deleteContact,
+};
 
 export default contactsService;
