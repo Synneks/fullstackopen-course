f2d8dc9b488f5e1b48e12aa573745e2a83f5f80b
Denis Eneotescu <denis.eneotescu@web-dienstleister.de>
2023-02-11 19:24:30 +0200

    part2 added footer component as well :'(

---
+++ part2-notes/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  const footerStyle = {
+    color: "green",
+    fontStyle: "italic",
+    fontSize: 16,
+  };
+  return (
+    <div style={footerStyle}>
+      <br /> <em>Note app, made by Synneks</em>
+    </div>
+  );
+};
+
+export default Footer;
