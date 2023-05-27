d9477492a0bd3fbd06b4d37f3eea2bd61efcf78a
Denis Eneotescu <deniseneotescu@gmail.com>
2023-05-27 16:05:58 +0300

    changed notes project structure

--- part3-notes-backend/models/note.js
+++ part3-notes-backend/models/note.js
@@ -1,19 +1,5 @@
 const mongoose = require('mongoose');
 
-const MONGO_DB_URL = String(process.env.MONGO_DB_URI);
-
-console.log('connecting to', MONGO_DB_URL);
-
-mongoose.set('strictQuery', false);
-mongoose
-  .connect(MONGO_DB_URL)
-  .then(() => {
-    console.log('connected to MongoDB');
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error);
-  });
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
