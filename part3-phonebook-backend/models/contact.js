f94b34635e559a0baa63f9804310740c71f27ffb
Denis Eneotescu <deniseneotescu@gmail.com>
2023-05-27 14:50:43 +0300

    part3:3-20_phonebook-database-step8 Added phone number validator

--- part3-phonebook-backend/models/contact.js
+++ part3-phonebook-backend/models/contact.js
@@ -22,8 +22,13 @@ const contactSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    minLength: 8,
-    require: true,
+    validate: {
+      validator: function (v) {
+        return /\d{3}-\d{3}-\d{4}/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, 'User phone number required'],
   },
 });
 
