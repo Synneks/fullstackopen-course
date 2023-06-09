e9fe2efe948c425f2bcd1b74db51b17e36d0ae92
Denis Eneotescu <deniseneotescu@gmail.com>
2023-06-09 16:13:26 +0300

    added integration tests to notes backend

--- part3-notes-backend/utils/config.js
+++ part3-notes-backend/utils/config.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 
 const PORT = process.env.PORT;
-const MONGO_DB_URI = process.env.MONGO_DB_URI;
+const MONGO_DB_URI =
+  process.env.NODE_ENV === 'test'
+    ? process.env.MONGO_DB_URI_TEST
+    : process.env.MONGODB_URI;
 
 module.exports = {
   MONGO_DB_URI,
