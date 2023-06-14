902b7913429749fb56bdac16343357a8743ebb2e
Denis Eneotescu <deniseneotescu@gmail.com>
2023-06-14 12:14:03 +0300

    part4:4-13_blog-list-expansions-1

--- part4-blog-backend/tests/controllers/blogs_api.test.js
+++ part4-blog-backend/tests/controllers/blogs_api.test.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../../app');
 const Blog = require('../../models/blog');
+const blogsRouter = require('../../controllers/blogs_api');
 
 const api = supertest(app);
 
@@ -35,68 +36,86 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-test('return all blogs as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-});
-
-test('the unique identifier property of the blog posts is named id', async () => {
-  const apiResult = await api.get('/api/blogs');
-
-  const blogs = JSON.parse(apiResult.text);
-
-  blogs.forEach((blog) => {
-    expect(blog.id).toBeDefined;
+describe('fetching blogs', () => {
+  test('return all blogs as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
   });
 });
 
-test('saving a new blog', async () => {
-  const newBlog = {
-    title: 'TSAONGAF',
-    author: 'MMANSON',
-    url: 'mmanson.com',
-    likes: 100,
-  };
+describe('blogs schema', () => {
+  test('the unique identifier property of the blog posts is named id', async () => {
+    const apiResult = await api.get('/api/blogs');
 
-  await api.post('/api/blogs').send(newBlog).expect(201);
+    const blogs = JSON.parse(apiResult.text);
 
-  const savedBlogs = await Blog.find({ title: 'TSAONGAF' });
-
-  expect(savedBlogs).toHaveLength(1);
-  expect(savedBlogs[0]).toMatchObject(newBlog);
+    blogs.forEach((blog) => {
+      expect(blog.id).toBeDefined;
+    });
+  });
 });
 
-test('saving a blog without likes defaults to 0', async () => {
-  const newBlog = {
-    title: 'TSAONGAF',
-    author: 'MMANSON',
-    url: 'mmanson.com',
-  };
+describe('saving blogs', () => {
+  test('saving a new blog', async () => {
+    const newBlog = {
+      title: 'TSAONGAF',
+      author: 'MMANSON',
+      url: 'mmanson.com',
+      likes: 100,
+    };
 
-  await api.post('/api/blogs').send(newBlog).expect(201);
+    await api.post('/api/blogs').send(newBlog).expect(201);
 
-  const savedBlogs = await Blog.find({ title: 'TSAONGAF' });
+    const savedBlogs = await Blog.find({ title: 'TSAONGAF' });
 
-  expect(savedBlogs).toHaveLength(1);
-  expect(savedBlogs[0].likes).toEqual(0);
+    expect(savedBlogs).toHaveLength(1);
+    expect(savedBlogs[0]).toMatchObject(newBlog);
+  });
+
+  test('saving a blog without likes defaults to 0', async () => {
+    const newBlog = {
+      title: 'TSAONGAF',
+      author: 'MMANSON',
+      url: 'mmanson.com',
+    };
+
+    await api.post('/api/blogs').send(newBlog).expect(201);
+
+    const savedBlogs = await Blog.find({ title: 'TSAONGAF' });
+
+    expect(savedBlogs).toHaveLength(1);
+    expect(savedBlogs[0].likes).toEqual(0);
+  });
+
+  test('saving a blog without title should return 400', async () => {
+    const newBlog = {
+      author: 'MMANSON',
+      url: 'mmanson.com',
+    };
+
+    await api.post('/api/blogs').send(newBlog).expect(400);
+  });
+
+  test('saving a blog without url should return 400', async () => {
+    const newBlog = {
+      title: 'test',
+      author: 'MMANSON',
+    };
+
+    await api.post('/api/blogs').send(newBlog).expect(400);
+  });
 });
 
-test('saving a blog without title should return 400', async () => {
-  const newBlog = {
-    author: 'MMANSON',
-    url: 'mmanson.com',
-  };
+describe('deleting blogs', () => {
+  test('deleting a blog works', async () => {
+    const savedBlogs = await Blog.find({});
 
-  await api.post('/api/blogs').send(newBlog).expect(400);
-});
-
-test('saving a blog without url should return 400', async () => {
-  const newBlog = {
-    title: 'test',
-    author: 'MMANSON',
-  };
-
-  await api.post('/api/blogs').send(newBlog).expect(400);
+    const blogToDelete = savedBlogs[0];
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+  });
+  test('deleting a blog that does not exist', async () => {
+    await api.delete(`/api/blogs/fakeId}`).expect(400);
+  });
 });
