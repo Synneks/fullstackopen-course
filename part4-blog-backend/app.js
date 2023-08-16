fb21ca7d646d7044bd3eb40c13d37449ff72fb66
Denis Eneotescu <deniseneotescu@gmail.com>
2023-08-16 19:40:08 +0300

    part4:4-15_blog-list-expansions-step-3

--- part4-blog-backend/app.js
+++ part4-blog-backend/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('express-async-errors');
 const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs_api');
+const usersRouter = require('./controllers/users_api');
 const config = require('./utils/config');
 const middleware = require('./utils/middleware');
 
@@ -25,6 +26,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 
 app.use(middleware.errorHandler);
 app.use(middleware.unknownEndpoint);
