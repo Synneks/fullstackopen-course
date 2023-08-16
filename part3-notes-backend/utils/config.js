d8e7299af934e8e24436fa96f278975212f2aeed
Denis Eneotescu <deniseneotescu@gmail.com>
2023-08-16 19:24:23 +0300

    notes be: token authentication

--- part3-notes-backend/utils/config.js
+++ part3-notes-backend/utils/config.js
@@ -6,7 +6,10 @@ const MONGO_DB_URI =
     ? process.env.MONGO_DB_URI_TEST
     : process.env.MONGO_DB_URI;
 
+const SECRET = process.env.SECRET;
+
 module.exports = {
   MONGO_DB_URI,
   PORT,
+  SECRET,
 };
