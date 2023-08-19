b116c03519b69e1713daf9dcf03ec42133c3e781
Denis Eneotescu <deniseneotescu@gmail.com>
2023-08-19 13:48:16 +0300

    part4:4-17_blog-list-expansions-step-5

--- part4-blog-backend/controllers/blogs_api.js
+++ part4-blog-backend/controllers/blogs_api.js
@@ -1,16 +1,25 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user');
   response.json(blogs);
 });
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body);
+  const defaultUser = await User.findOne({});
 
-  const result = await blog.save();
-  response.status(201).json(result);
+  const blog = new Blog({
+    ...request.body,
+    user: defaultUser.id,
+  });
+
+  const savedBlog = await blog.save();
+  defaultUser.blogs = [...defaultUser.blogs, savedBlog.id];
+  await defaultUser.save();
+
+  response.status(201).json(savedBlog);
 });
 
 blogsRouter.delete('/:id', async (request, response) => {
