ff43f0844f2a4cab6acec8188c60fc1fde11ef4e
Denis Eneotescu <deniseneotescu@gmail.com>
2023-08-19 13:07:16 +0300

    part4:4-16_blog-list-expansions-step-4

--- part4-blog-backend/models/user.js
+++ part4-blog-backend/models/user.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   name: String,
-  passwordHash: String,
+  passwordHash: {
+    type: String,
+    required: true,
+  },
 });
 
 userSchema.set('toJSON', {
@@ -14,6 +22,8 @@ userSchema.set('toJSON', {
     delete returnedObject.passwordHash;
   },
 });
+
+userSchema.plugin(uniqueValidator);
 
 const User = mongoose.model('User', userSchema);
 
---
+++ part4-blog-backend/tests/controllers/users_api.test.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const bcrypt = require('bcrypt');
+const app = require('../../app');
+const User = require('../../models/user');
+const helper = require('../test_utils/test_helper');
+
+const api = supertest(app);
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+
+    const passwordHash = await bcrypt.hash('sekret', 10);
+    const user = new User({
+      username: 'root',
+      passwordHash,
+    });
+
+    await user.save();
+  });
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: 'newUser',
+      name: 'New User',
+      password: 'password',
+    };
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
+
+    const usernames = usersAtEnd.map((u) => u.username);
+    expect(usernames).toContain(newUser.username);
+  });
+
+  test('creation fails with same username', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    await api.post('/api/users').send(usersAtStart[0]).expect(400);
+  });
+
+  test('creation fails with short username', async () => {
+    const newUser = {
+      username: '12',
+      name: '',
+      password: '12',
+    };
+
+    await api.post('/api/users').send(newUser).expect(400);
+  });
+});
