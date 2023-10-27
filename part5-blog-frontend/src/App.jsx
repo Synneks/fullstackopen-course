95b460c38a0f6956ac94fd93c060ee1553b7b8ec
Denis Eneotescu <deniseneotescu@gmail.com>
2023-10-27 15:22:38 +0300

    part5:5-5_blog-list-frontend-step-5

--- part5-blog-frontend/src/App.jsx
+++ part5-blog-frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Blog from './components/Blog';
 import blogService from './services/blogs';
 import loginService from './services/login';
 import Notification from './components/Notification';
+import Togglable from './components/Togglable';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
@@ -53,30 +54,32 @@ const App = () => {
   };
 
   const loginForm = () => (
-    <div>
-      <h2>Log in to application</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          username:
-          <input
-            type="text"
-            value={username}
-            name="username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          password:
-          <input
-            type="password"
-            value={password}
-            name="password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit"> login </button>
-      </form>
-    </div>
+    <Togglable buttonLabel="Log in">
+      <div>
+        <h2>Log in to application</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            Username:
+            <input
+              type="text"
+              value={username}
+              name="username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            Password:
+            <input
+              type="password"
+              value={password}
+              name="password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">Log In</button>
+        </form>
+      </div>
+    </Togglable>
   );
 
   const handleCreateForm = async (event) => {
